3f064f6ec0cdf1de8e4219d35ca0d88aa351d75e
eparriel <enzo.parriel@gmail.com>
2025-01-13 00:11:43 +0100

    fix: Clear

--- includes/command/Clear.h
+++ includes/command/Clear.h
@@ -6,9 +6,6 @@
 
 class Clear : public ICommand {
 public:
-    void execute(const QList<Token> &tokens) override {
-        // Handle INDEXER commands like START, STOP, STATUS, etc.
-        qDebug() << "Executing CLEAR command with tokens:" << tokens;
-    }
+    void execute(const QList<Token> &tokens) override;
 };
 #endif //CLEAR_H
